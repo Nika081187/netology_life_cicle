5d928077b4d6c69d57f6b6e76e7605b23026fc22
Veronika Milchakova <v.milchakova@corp.mail.ru>
2021-01-11 20:47:18 +0300

    first commit

---
+++ Profile/PhotosTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  PhotosTableViewCell.swift
+//  Navigation
+//
+//  Created by v.milchakova on 16.12.2020.
+//  Copyright © 2020 Artem Novichkov. All rights reserved.
+//
+
+import UIKit
+
+class PhotosTableViewCell: UITableViewCell {
+    private lazy var photos = ProfilePhotoStackView()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        addSubview(photos)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
